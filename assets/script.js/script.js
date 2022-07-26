8c7a6d04561d20176b95918c807db362c156afc3
unknown <jadentr.44@gmail.com>
2022-07-26 00:21:24 -0500

    will show the name of the links

--- assets/script.js/script.js
+++ assets/script.js/script.js
@@ -1,3 +1,14 @@
-document.querySelectorAll('.hover').addEventListener('mouseover',element=>{
-  console.log()
+$( ".hover" ).hover(function() {
+  let parent =  $(this).parent()
+  let child = $(this).parent().children().eq(1)
+
+  if(parent.data('link') === 'true'){
+    
+    child.text(child.attr('id'))
+    parent.data('link','false')
+  }else{
+    child.text($(this).attr('id'))
+    parent.data('link','true')
+  }
+  
 })
