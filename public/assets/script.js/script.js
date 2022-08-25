cc5086647bd14a9cfb54e0528d145fcc03dc9247
Jaden <jadentr.44@gmail.com>
2022-08-24 21:31:00 -0500

    troubleshooting

--- public/assets/script.js/script.js
+++ public/assets/script.js/script.js
@@ -31,7 +31,6 @@ function render() {
 // dots class
 // @constructor
 function dot() {
-  
   this.rad_x = 2 * Math.random() * halfx + 1;
   this.rad_y = 1.2 * Math.random() * halfy + 1;
   this.alpha = Math.random() * 360 + 1;
@@ -39,25 +38,21 @@ function dot() {
   this.speed *= 0.1;
   this.size = Math.random() * 10 + 1;
   this.color = Math.floor(Math.random() * 256);
-  
 }
 
 // drawing dot
-dot.prototype.draw = function() {
-  
+dot.prototype.draw = function () {
   // calc polar coord to decart
-  var dx = halfx + this.rad_x * Math.cos(this.alpha / 180 * Math.PI);
-  var dy = halfy + this.rad_y * Math.sin(this.alpha / 180 * Math.PI);
+  var dx = halfx + this.rad_x * Math.cos((this.alpha / 180) * Math.PI);
+  var dy = halfy + this.rad_y * Math.sin((this.alpha / 180) * Math.PI);
   // set color
   context.fillStyle = "white";
   // draw dot
-  context.fillRect(dx, dy, this.size/2, this.size/2);
-  
+  context.fillRect(dx, dy, this.size / 2, this.size / 2);
 };
 
 // calc new position in polar coord
-dot.prototype.move = function() {
-  
+dot.prototype.move = function () {
   this.alpha += this.speed;
   // change color
   if (Math.random() * 100 < 50) {
@@ -65,186 +60,169 @@ dot.prototype.move = function() {
   } else {
     this.color -= 1;
   }
-  
 };
 
 // start animation
 render();
 // hover affect on welcome
 
-$('.socialHover').hover(function(e){
-  console.log(e.target)
+$(".socialHover").hover(function (e) {
+  console.log(e.target);
   let link = e.target.id;
   let text;
-  let linkStatus = $(e.target).data('link');
+  let linkStatus = $(e.target).data("link");
 
-  if(linkStatus){
+  if (linkStatus) {
     //changing to the original text
 
     //toggle the state
-    $(e.target).data('link',false)
+    $(e.target).data("link", false);
 
     //changing linkedin back
-    if(link === 'github') {
-      text = $('#gitText')
-      text.css('color',"white")
-      text.text("Jaden")
+    if (link === "github") {
+      text = $("#gitText");
+      text.css("color", "white");
+      text.text("Jaden");
     }
     //changing linkedin back
-    if(link === 'linkedin') {
-      text = $('#LN-Text')
-      text.css('color',"white")
-      text.text("Rodriguez")
+    if (link === "linkedin") {
+      text = $("#LN-Text");
+      text.css("color", "white");
+      text.text("Rodriguez");
     }
     //changing linkedin back
-    if(link === 'email') {
-      text = $('#emailText')
-      text.css('color',"white")
-      text.text("JS Developer")
+    if (link === "email") {
+      text = $("#emailText");
+      text.css("color", "white");
+      text.text("JS Developer");
     }
-  }else{
-    //change text to the link name 
+  } else {
+    //change text to the link name
 
     //toggle the state
-    $(e.target).data('link',true)
+    $(e.target).data("link", true);
 
     //show moving code when linkedin is hovered
-    if(link === 'github') {
-      text = $('#gitText')
-      text.css('color',"rgb(0,0,0,.0)")
-      text.text("Github")
+    if (link === "github") {
+      text = $("#gitText");
+      text.css("color", "rgb(0,0,0,.0)");
+      text.text("Github");
     }
-    if(link === 'linkedin') {
-      text = $('#LN-Text')
-      text.css('color',"rgb(0,0,0,.0)")
-      text.text("Linkedin")
+    if (link === "linkedin") {
+      text = $("#LN-Text");
+      text.css("color", "rgb(0,0,0,.0)");
+      text.text("Linkedin");
     }
-    if(link === 'email') {
-      text = $('#emailText')
-      text.css('color',"rgb(0,0,0,.0)")
-      text.text("email")
+    if (link === "email") {
+      text = $("#emailText");
+      text.css("color", "rgb(0,0,0,.0)");
+      text.text("email");
     }
-
   }
-})
-
-
-
-
-
-let skillState = false
-$( ".skill" ).hover(function() {
-  if(!skillState){
-
-    let skillName = $(this).attr("id")
-    $('.skillName').text(skillName)
-    $('.skillName').removeClass("invisible")
-    $('.skillName').addClass("visible")
-  }else{
-    $('.skillName').addClass("invisible")
-    $('.skillName').removeClass("visible")
-  }
-  skillState = !skillState
-})
-
-
-
-function floatIn(text,divID,time){
-let div = $(`#${divID}`)
-let textArr = text.split("")
-
-textArr.forEach((e,i) => {
-  setTimeout(() => {
-    
-    let span = $("<span>")
-    span.text(e)
-    span.animate({fontSize: "100%" }, time );
-    // span.addClass('big')
-    // setTimeout(() => {span.removeClass('big')},200)
-    div.append(span)
-  }, time/2*i)
 });
 
+let skillState = false;
+$(".skill").hover(function () {
+  if (!skillState) {
+    let skillName = $(this).attr("id");
+    $(".skillName").text(skillName);
+    $(".skillName").removeClass("invisible");
+    $(".skillName").addClass("visible");
+  } else {
+    $(".skillName").addClass("invisible");
+    $(".skillName").removeClass("visible");
+  }
+  skillState = !skillState;
+});
 
-console.log(textArr)
+function floatIn(text, divID, time) {
+  let div = $(`#${divID}`);
+  let textArr = text.split("");
+
+  textArr.forEach((e, i) => {
+    setTimeout(() => {
+      let span = $("<span>");
+      span.text(e);
+      span.animate({ fontSize: "100%" }, time);
+      // span.addClass('big')
+      // setTimeout(() => {span.removeClass('big')},200)
+      div.append(span);
+    }, (time / 2) * i);
+  });
+
+  console.log(textArr);
 }
 
-
-
-
-
-floatIn("jaden","gitText",450)
-floatIn("Rodriguez","LN-Text",280)
-floatIn("JS Developer","emailText",200)
-
-
+floatIn("jaden", "gitText", 450);
+floatIn("Rodriguez", "LN-Text", 280);
+floatIn("JS Developer", "emailText", 200);
 
 // fetch to send email
 
-$('#sendEmail').on('click',function(e){
+$("#sendEmail").on("click", function (e) {
   e.preventDefault();
   // regex
   let emailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
   // checking if input is not valid
-  if(!($('.emailInput').val().match(emailformat)) || !$('#message').val() || !$('#name').val()){
-    $('#formError').css('visibility','visible')
-    return
+  if (
+    !$(".emailInput").val().match(emailformat) ||
+    !$("#message").val() ||
+    !$("#name").val()
+  ) {
+    $("#formError").css("visibility", "visible");
+    return;
   }
 
   // hiding 'invalid input' is the input was good
-  $('#formError').css('visibility','hidden')
+  $("#formError").css("visibility", "hidden");
 
-
-  let name = $('#name').val()
-  let email = $('.emailInput').val()
-  let message = $('#message').val()
-  console.log(name)
+  let name = $("#name").val();
+  let email = $(".emailInput").val();
+  let message = $("#message").val();
+  console.log(name);
   console.log(email);
   console.log(message);
   //sending post to server to send email
-  fetch('/sendEmail', {
-    method: 'POST',
+  fetch("/sendEmail", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({name:name,email:email,message:message}),
+    body: JSON.stringify({ name: name, email: email, message: message }),
   })
     .then((res) => res.json())
     .then((data) => {
       // clearing inputs and alerting the email was good
-      console.log(data)
-      alert('Successful email sent');
-      $('#name').val("")
-      $('.emailInput').val("")
-      $('#message').val("")
-      return 
+      console.log(data);
+      alert("Successful email sent");
+      $("#name").val("");
+      $(".emailInput").val("");
+      $("#message").val("");
+      return;
     })
     .catch((error) => {
       // if theres an error alert the bad email and console log the error
-      alert('Error in sending email');
-      console.log(error)
+      alert("Error in sending email");
+      console.log(error);
     });
-  
-})
-
+});
 
 let projectsInfo = {
-  Dart:{
-    name:"JavaScript Quiz",
-    desc:"a simple game that useses vanila javascript to quiz your javavacript syntax knolage",
-    full:"no",
-    githubLink:"",
-    liveLink:''
-  }
-}
+  Dart: {
+    name: "JavaScript Quiz",
+    desc: "a simple game that useses vanila javascript to quiz your javavacript syntax knolage",
+    full: "no",
+    githubLink: "",
+    liveLink: "",
+  },
+};
 
-$('.card').on('click',function(e){
-  console.log(e.currentTarget.id)
-  let info = projectsInfo[e.currentTarget.id]
-  $('#modalTitle').text(info.name)
-  $('#modalFull').text(info.full)
-  $('#modalDesc').text(info.desc)
-  
-  $('#myModal').modal('show'); 
-})
+$(".card").on("click", function (e) {
+  let info = projectsInfo[e.currentTarget.id];
+  $("#modalTitle").text(info.name);
+  $("#modalFull").text(info.full);
+  $("#modalDesc").text(info.desc);
+
+  $("#myModal").modal("show");
+});
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 
 //api call to send email to me
