222617c158dcd7a688dc27baf9e72f6537d09bbb
Jaden <jadentr.44@gmail.com>
2022-08-24 21:42:23 -0500

    i trust andrew that this will work

--- server.js
+++ server.js
@@ -6,14 +6,17 @@ require('dotenv').config();
 // Helper method for generating unique ids
 
 const PORT = process.env.PORT || 5000;
-
+const publicPath = path.join(process.cwd(), 'project')
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('project'));
+app.use(express.static(publicPath));
 
+app.get('/', (req,res) =>{
+  res.sendFile(path.join(publicPath, 'index.html'))
+})
 
 //api call to send email to me
 app.post('/sendEmail', (req, res) =>{
