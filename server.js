92c9ba75a634149e15d7a81f6e873ba8ba0035fa
Jaden <jadentr.44@gmail.com>
2022-08-24 20:39:06 -0500

    changed port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 // Helper method for generating unique ids
 
-const PORT = 3001;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -14,10 +14,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
+app.get('/', (req, res) => {
+  res.send(
+    `<p>API - An application programming interface, is a computing interface that defines interactions between multiple software intermediaries</p>`
+  );
+});
 //api call to send email to me
 app.post('/sendEmail', (req, res) =>{
 let emailInfo = req.body
